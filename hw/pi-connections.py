b28a7120de01753ec160c92adb1994c318a99b43
programmer-ngani <chingudachi00@gmail.com>
2024-10-17 16:30:45 +0900

    Create pi-connections.py

---
+++ hw/pi-connections.py
@@ -0,0 +1,38 @@
+Table 1. Pin Connection of Raspberry Pi and Relay Module
+
+Raspberry Pi	Relay
+Board	Label	Module
+17	3.3V	JDVCC
+39	GND	GND
+37		IN1
+35		IN2
+33		IN3
+31		IN4
+29		IN5
+
+
+Table 2. Pin Connection of Raspberry Pi and Limit Switch
+
+Raspberry Pi	Limit
+Board	Label	Switch
+30	GND	GND
+32		LS-D1
+14	GND	GND
+16		LS-D2
+18		LS-D3
+20	GND	GND
+22		LS-D4
+24		LS-D5
+
+
+Table 3. Pin Connection of Raspberry Pi and Ultrasonic Sensor Module
+
+Raspberry Pi	Ultrasonic	Voltage	
+Board	Label	Sensor Module	Divider	
+6	GND	US-GND		
+4	5V	US-VCC		
+13		US-TRIG		
+None		US-ECHO		VD-IN
+11				VD-ECHO
+9	GND			VD-GND
+
