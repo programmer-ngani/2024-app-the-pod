9fca3feb1dab120d0e5a7a83cd4ff219edcf7285
programmer-ngani <chingudachi00@gmail.com>
2024-10-27 17:24:55 +0800

    Create pi-operations-c.py

---
+++ sw/pi-operations-c.py
@@ -0,0 +1,36 @@
+Monitor an Input Switch to Control an LED.
+
+Pre-requisite:
+1) Control a GPIO pin to Blink an LED.
+
+Steps:
+6) Use Terminal Commands
+6.1) <pwd> for showing the present working directory
+6.2) <ls> or <ls -a> for listing sub-directories and files in the current directory
+6.3) touch <btn.py> for creating a python file
+6.4) sudo nano <btn.py> for editing or creating a file
+
+7) Button Script (https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/)
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep # Import the sleep function from the time module
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # Set pin 18 to be an output pin and set initial value to low (off)
+
+while True:
+    if GPIO.input(10) == GPIO.LOW:
+        print("Button was pushed!")
+        GPIO.output(18, GPIO.HIGH) # Turn on
+    else:
+        GPIO.output(18, GPIO.LOW) # Turn off
+    sleep(1) # Sleep for 1 second
+
+7.1) <Ctrl+X> to exit editing.
+7.2) Optional to rename file.
+7.3) Type <Y> of <N> to save or cancel.
+
+Next:
+Control a Relay Module using Raspberry Pi
