fe5a36b6cb4d3e8b329a7bf5135e74d2d0a304b5
programmer-ngani <chingudachi00@gmail.com>
2024-10-27 16:46:12 +0800

    Update pi-ref-links.py

--- sw/pi-ref-links.py
+++ sw/pi-ref-links.py
@@ -5,3 +5,6 @@ https://www.youtube.com/watch?v=HxtjAxsoiAs
 
 2) Blink LED using Python
 https://www.hackster.io/rajeshjiet/led-blink-using-raspberry-pi-b38dbe
+
+3) Using a Push Button with Raspberry Pi GPIO
+https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
