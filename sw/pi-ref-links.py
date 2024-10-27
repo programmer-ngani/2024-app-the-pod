3f4c819c6f1731fc354d22add511f26e1082473e
programmer-ngani <chingudachi00@gmail.com>
2024-10-27 21:23:37 +0800

    Update pi-ref-links.py

--- sw/pi-ref-links.py
+++ sw/pi-ref-links.py
@@ -9,5 +9,11 @@ https://www.hackster.io/rajeshjiet/led-blink-using-raspberry-pi-b38dbe
 3) Using a Push Button with Raspberry Pi GPIO
 https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
 
-4) Raspberry Pico W
+4) Blink LED using Micropython (Raspberry Pi Pico W)
+https://diyprojectslab.com/raspberry-pi-pico-w-led-blink-micropython/
+
+5) Using a Push Button (Raspberry Pi Pico W)
+https://www.hackster.io/Ramji_Patel/raspberry-pi-pico-and-button-321059
+
+6) Control LED and Read Board Temperature of Raspberry Pi Pico W
 https://www.hackster.io/rajivcodelab/how-to-build-a-rest-api-on-raspberry-pi-pico-w-control-led-91b25b
