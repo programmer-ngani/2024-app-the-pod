73947165b87ba080ce6823573dabbb82fa680a78
programmer-ngani <chingudachi00@gmail.com>
2024-10-27 17:44:22 +0800

    Update pi-ref-links.py

--- sw/pi-ref-links.py
+++ sw/pi-ref-links.py
@@ -8,3 +8,6 @@ https://www.hackster.io/rajeshjiet/led-blink-using-raspberry-pi-b38dbe
 
 3) Using a Push Button with Raspberry Pi GPIO
 https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
+
+4) Raspberry Pico W
+https://www.hackster.io/rajivcodelab/how-to-build-a-rest-api-on-raspberry-pi-pico-w-control-led-91b25b
