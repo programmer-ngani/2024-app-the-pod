02e26ce2ffd7e8f0dea9fc18a98251a74199b598
programmer-ngani <chingudachi00@gmail.com>
2024-10-27 15:49:15 +0800

    Update pi-ref-links.py

--- sw/pi-ref-links.py
+++ sw/pi-ref-links.py
@@ -2,3 +2,6 @@ References:
 
 1) Raspberry Pi Terminal Commands
 https://www.youtube.com/watch?v=HxtjAxsoiAs
+
+2) Blink LED using Python
+https://www.hackster.io/rajeshjiet/led-blink-using-raspberry-pi-b38dbe
