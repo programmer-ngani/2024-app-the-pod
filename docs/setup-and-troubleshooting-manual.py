0189ab69004f36ed7c16e2e58e436c44747d98e1
programmer-ngani <chingudachi00@gmail.com>
2024-10-28 19:31:45 +0900

    Update setup-and-troubleshooting-manual.py

--- docs/setup-and-troubleshooting-manual.py
+++ docs/setup-and-troubleshooting-manual.py
@@ -1,6 +1,10 @@
 System Configuration
 
 A. Developer
+0) Download and Install
+0.1) Termius or Putty SSH
+0.2) Angry IP Scanner or Advanced IP Scanner
+0.3) Postman
 1) Using Thonny IDE
 1.1) Change wifi credentials.
 1.2) Create or edit <secrets.py>, add the ff:
@@ -13,6 +17,11 @@ MY_DEVICE_UID = "mduid-x"
 MY_DEVICE_UID = "mduid-1"
 #or
 MY_DEVICE_UID = "mduid-2"
+2) Troubleshooting Tips
+2.1) Using Thonny IDE, monitor messages on Shell.
+2.1.1) IP Address will be shown on Thonny IDE Shell.
+2.2) Using IP Scanner, scan network to discover devices.
+2.2.1) Connected devices and IP Address will be shown.
 
 B. Owner
 0) The whole system is powered off.
