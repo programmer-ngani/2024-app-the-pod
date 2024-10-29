94f5e28de35121f6c2baefe8e4148840b3a97f68
programmer-ngani <chingudachi00@gmail.com>
2024-10-29 23:55:53 +0900

    Create setup-manual-for-users.py

---
+++ docs/setup-manual-for-users.py
@@ -0,0 +1,16 @@
+Pre-requisite:
+A. User Guide for Developer
+
+System Configuration:
+B. User Guide for Owner
+0) The whole system is powered off.
+1) First, turn on the wireless router (network).
+2) Then, turn on the Raspberry Pi Pico W one-by-one.
+2.1) LED is on after startup.
+2.2) LED is off once connected to network.
+3) Lastly, turn on the Raspberry Pi Zero 2W.
+3.1) LED is blinking after startup.
+3.2) LED is on once connected to network.
+
+C. User Guide for Customer
+0) Download and install the Android Application.
