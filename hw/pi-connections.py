f86d1a8c431a6f5a6f18cc2d37457497e003c38c
programmer-ngani <chingudachi00@gmail.com>
2024-11-07 19:45:56 +0800

    Update pi-connections.py

--- hw/pi-connections.py
+++ hw/pi-connections.py
@@ -1,4 +1,4 @@
-Table 1. Pin Connection of Raspberry Pi and Relay Module
+Table 1. Pin Connection of Raspberry Pi (Zero) and Relay Module
 
 Raspberry Pi	Relay
 Board	Label	Module
@@ -11,7 +11,7 @@ Board	Label	Module
 29		IN5
 
 
-Table 2. Pin Connection of Raspberry Pi and Limit Switch
+Table 2. Pin Connection of Raspberry Pi (Zero) and Limit Switch
 
 Raspberry Pi	Limit
 Board	Label	Switch
@@ -25,7 +25,7 @@ Board	Label	Switch
 24		LS-D5
 
 
-Table 3. Pin Connection of Raspberry Pi and Ultrasonic Sensor Module
+Table 3. Pin Connection of Raspberry Pi (Zero) and Ultrasonic Sensor Module
 
 Raspberry Pi	Ultrasonic	Voltage	
 Board	Label	Sensor Module	Divider	
@@ -35,4 +35,45 @@ Board	Label	Sensor Module	Divider
 None		US-ECHO		VD-IN
 11				VD-ECHO
 9	GND			VD-GND
+
+
+Table 4. Pin Connection of Raspberry Pi (Pico) and Relay Module
+
+Raspberry Pi	Relay
+Board	Label	Module
+36	3.3V	JDVCC
+38	GND	GND
+21		IN1
+22		IN2
+24		IN3
+25		IN4
+26		IN5
+27		IN6
+
+
+Table 5. Pin Connection of Raspberry Pi (Pico) and Limit Switch
+
+Raspberry Pi	Limit
+Board	Label	Switch
+3	GND	GND
+4		LS-D1
+8	GND	GND
+9		LS-D2
+10		LS-D3
+13	GND	GND
+14		LS-D4
+15		LS-D5
+
+
+Table 6. Pin Connection of Raspberry Pi (Pico) and Ultrasonic Sensor Module
+
+Raspberry Pi	Ultrasonic	Voltage	
+Board	Label	Sensor Module	Divider	
+G	GND	US-GND		
+V	5V	US-VCC		
+T		US-TRIG		
+None		US-ECHO		VD-IN
+E				VD-ECHO
+G	GND			VD-GND
+
 
